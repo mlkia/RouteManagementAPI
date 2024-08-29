78c0307653543c8fdfb0cb69efa91f219af1ae82
Malik E.A <mlk_ea@hotmail.com>
2024-08-29 18:57:55 +0200

    Add DriverID to Route and enhance RouteService

--- RouteManagementTutorial/Services/RouteService.cs
+++ RouteManagementTutorial/Services/RouteService.cs
@@ -4,19 +4,23 @@ using Entities;
 using RouteManagementTutorial.Models;
 using RouteManagementTutorial.Helper;
 using RouteManagementTutorial.DTO;
-using Route = Entities.Route; //This is to avoid confusion with the Route class in the 'Microsoft.AspNetCore.Routing.Route'
+using Route = Entities.Route;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc; //This is to avoid confusion with the Route class in the 'Microsoft.AspNetCore.Routing.Route'
 
 namespace RouteManagementTutorial.Services
 {
     public class RouteService
     {
         private readonly IMongoCollection<Route> _routesCollection;
-        public RouteService(IOptions<RouteManagementTutorialDataBaseSettings> RMTDataBasesettings)
+        private readonly DriversService _driversService;
+        public RouteService(IOptions<RouteManagementTutorialDataBaseSettings> RMTDataBasesettings, DriversService driversService)
         {
             var mongoclient = new MongoClient(RMTDataBasesettings.Value.ConnectionString);
             var database = mongoclient.GetDatabase(RMTDataBasesettings.Value.DatabaseName);
 
             _routesCollection = database.GetCollection<Route>(RMTDataBasesettings.Value.RoutesCollectionName);
+            _driversService = driversService;
         }
 
         public async Task<List<Route>> GetAsync() =>
@@ -25,7 +29,29 @@ namespace RouteManagementTutorial.Services
         public async Task<Route?> GetAsync(string id) =>
             await _routesCollection.Find(x => x.RouteId == id).FirstOrDefaultAsync();
 
-        public async Task CreateAsync(Route newRoute) =>
+        public async Task<CreateRouteResult> CreateAsync(Route newRoute)
+        {
+            var driver = await _driversService.GetAsync(newRoute.DriverID);
+
+            if (driver is null)
+            {
+                return new CreateRouteResult { Success = false, Message= "Driver Not Found" }; 
+            }
+
+            newRoute.DriverName = driver.Name;
+
             await _routesCollection.InsertOneAsync(newRoute);
+
+            return new CreateRouteResult { Success = true, Route = newRoute };
+        }
+
     }
+
+    public class CreateRouteResult
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public Route? Route { get; set; }
+    }
+
 }
